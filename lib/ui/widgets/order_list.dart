db04bdbf72594a50da1c38d08ddaa921455d8528
Tio Irawan <sattanationmail@gmail.com>
2021-08-13 12:42:04 +0700

    Refactor order list name

--- lib/ui/widgets/order_list.dart
+++ lib/ui/widgets/order_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:seller/ui/common/styles.dart';
-import 'package:seller/ui/widgets/item_checker.dart';
+
+import 'order_list_item.dart';
 
 class OrderList extends StatelessWidget {
   const OrderList({Key? key}) : super(key: key);
@@ -9,65 +9,9 @@ class OrderList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        OrderItem(),
-        OrderItem(),
+        OrderListItem(),
+        OrderListItem(),
       ],
-    );
-  }
-}
-
-class OrderItem extends StatelessWidget {
-  const OrderItem({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CircleAvatar(
-                radius: 30,
-                backgroundImage: NetworkImage(
-                  'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Pesanan Baru",
-                    style: kTextTheme.headline5,
-                  ),
-                  Text("Driver : Adecya Jalu"),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.schedule,
-                        size: kTextTheme.overline!.fontSize,
-                      ),
-                      SizedBox(width: 4),
-                      Text(
-                        "22 Jul; 08:30",
-                        style: kTextTheme.overline,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              ElevatedButton(onPressed: () {}, child: Text("Hubungi"))
-            ],
-          ),
-          Divider(
-            thickness: 1,
-          ),
-          OrderItemChecker(),
-        ],
-      ),
     );
   }
 }
---
+++ lib/ui/widgets/order_list_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:seller/ui/common/styles.dart';
+
+import 'order_item_checker.dart';
+
+class OrderListItem extends StatelessWidget {
+  const OrderListItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(
+                  'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Pesanan Baru",
+                    style: kTextTheme.headline5,
+                  ),
+                  Text("Driver : Adecya Jalu"),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.schedule,
+                        size: kTextTheme.overline!.fontSize,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        "22 Jul; 08:30",
+                        style: kTextTheme.overline,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Hubungi"))
+            ],
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          OrderItemChecker(),
+        ],
+      ),
+    );
+  }
+}
